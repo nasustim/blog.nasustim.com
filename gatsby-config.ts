45df23c83787712d7995af0542a429b30d4465f1
nasustim <mitsuhibino@gmail.com>
2024-08-21 03:45:51 +0900

    fix: broken setting

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,5 @@
 import type { GatsbyConfig } from "gatsby";
+import { GTAG_TRACKING_ID } from "./src/config";
 
 const config: GatsbyConfig = {
   siteMetadata: {
@@ -9,7 +10,14 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-vanilla-extract", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-vanilla-extract", {
+    resolve: "gatsby-plugin-google-gtag",
+    options: {
+      trackingIds: [
+        GTAG_TRACKING_ID,
+      ],
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const GTAG_TRACKING_ID = 'G-3YY246MS11'
