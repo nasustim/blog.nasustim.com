abce5b16d8df4ab591c3969c8573d0e1869cbe93
nasustim <mitsuhibino@gmail.com>
2024-08-21 05:08:56 +0900

    fix: typings

--- src/pages/entry/{markdownRemark.frontmatter__slug}.tsx
+++ src/pages/entry/{markdownRemark.frontmatter__slug}.tsx
@@ -1,22 +1,25 @@
 import * as React from "react";
-import { graphql } from "gatsby";
+import { type PageProps, graphql } from "gatsby";
 
-export default function BlogPostTemplate({ data }) {
-	const { markdownRemark } = data;
-	const { frontmatter, html } = markdownRemark;
-	return (
-		<div>
-			<div>
-				<h1>{frontmatter.title}</h1>
-				<h2>{frontmatter.date}</h2>
-				<div dangerouslySetInnerHTML={{ __html: html }} />
-			</div>
-		</div>
-	);
+export const BlogPostTemplate: React.FC<PageProps<Queries.EntryPageQuery>> = ({ data }) => {
+  if (!data.markdownRemark) {
+    return <></>
+  }
+  const { frontmatter, html } = data.markdownRemark;
+
+  return (
+    <div>
+      <div>
+        <h1>{frontmatter?.title}</h1>
+        <h2>{frontmatter?.date}</h2>
+        <div dangerouslySetInnerHTML={{ __html: html ?? '' }} />
+      </div>
+    </div>
+  );
 }
 
 export const pageQuery = graphql`
-  query($id: String!) {
+  query EntryPage ($id: String!) {
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
