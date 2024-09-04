2dd6c339bcfaa7ed99bbae4c3d60f86f20b1066b
nasustim <mitsuhibino@gmail.com>
2024-09-05 00:04:50 +0900

    fix heading style

--- src/components/organisms/markdown-renderer/index.css.ts
+++ src/components/organisms/markdown-renderer/index.css.ts
@@ -22,6 +22,12 @@ globalStyle(`${articleRoot} > *`, {
 globalStyle("p img", {
 	maxWidth: "100%",
 });
-globalStyle("h1, h2, h3, h4, h5", {
-	fontWeight: "500",
-});
+for (let i = 1; i <= 5; i++) {
+  const tag = `h${i}`;
+  const fontWeight = 500 - (( i / 2) * 100)
+  const fontSize = 28 - (( i - 1) * 3)
+  globalStyle(tag, {
+    fontWeight,
+    fontSize,
+  });
+}
