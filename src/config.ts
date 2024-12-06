8f1273e4eea0868714a38ea000baa3ea083546e7
nasustim <mitsuhibino@gmail.com>
2024-12-06 20:07:59 +0900

    change threshold

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ const SITE_SCHEMA = isProd ? "https" : "http";
 const SITE_DOMAIN = isProd ? "blog.nasustim.com" : "localhost:8000";
 export const SITE_ORIGIN = `${SITE_SCHEMA}://${SITE_DOMAIN}`;
 
-export const ARTICLE_LIST_PAGE_LIMIT = 5;
+export const ARTICLE_LIST_PAGE_LIMIT = 10;
 
 /**
  * Google Tag Manager
