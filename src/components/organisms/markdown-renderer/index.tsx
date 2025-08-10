3ca4fbb6c898fbda8a7cbf5f410bc89764136ea3
nasustim <mitsuhibino@gmail.com>
2025-08-10 16:37:20 +0900

    Modify styling

--- src/components/organisms/markdown-renderer/index.tsx
+++ src/components/organisms/markdown-renderer/index.tsx
@@ -21,7 +21,9 @@ export const MarkdownRenderer: FC<Props> = ({
     <div className={blockStyle}>
       {date ? <small>{date}</small> : null}
       <h2 className={h2Style}>{title}</h2>
-      {tags.length > 0 && <TagList tags={tags} title="Tags: " />}
+      {tags.length > 0 && (
+        <TagList tags={tags} />
+      )}
     </div>
     <article className={`${blockStyle} ${articleRoot}`}>
       <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
